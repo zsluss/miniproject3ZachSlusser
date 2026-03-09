ce05ab3677bb28bfdf6f4cf2b885697844686a77
zsluss <zluss@gmail.com>
2026-03-09 12:42:47 -0500

    Implement user authentication with registration and login functionality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -25,5 +25,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
